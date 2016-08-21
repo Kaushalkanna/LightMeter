6c3dfaaf1247b98118a208ce8f9d2367c353e32b
kdontula <kdontula@hawk.iit.edu>
2016-08-21 15:00:03 -0500

    removed graph added min and max made ui changes

--- app/src/main/java/com/kaushal/lightmeter/LightListener.java
+++
@@ -1,8 +0,0 @@
-package com.kaushal.lightmeter;
-
-/**
- * Created by xkxd061 on 8/20/16.
- */
-public interface LightListener {
-    public void runGraph();
-}
--- app/src/main/java/com/kaushal/lightmeter/MainActivity.java
+++ app/src/main/java/com/kaushal/lightmeter/MainActivity.java
@@ -6,59 +6,32 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor lightSensor;
-    TextView tv1;
-    private final Handler handler = new Handler();
-    GraphView graph;
-    int graphXMin = 0;
-    int graphXMax = 250;
-    int graphYMin = 0;
-    int graphYMax = 120;
-    private LineGraphSeries<DataPoint> graphData;
-    private double graphLastXValue = -1d;
-    private double luxValue = 0;
+    TextView lux;
+    TextView maxLux;
+    TextView minLux;
+    private int luxValue = 0;
+    List<Integer> listValues = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv1 = (TextView) findViewById(R.id.textView);
-        graph = (GraphView) findViewById(R.id.graph);
-        graphData = new LineGraphSeries<DataPoint>();
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(graphXMin);
-        graph.getViewport().setMaxX(graphXMax);
-        graph.getViewport().setYAxisBoundsManual(true);
-        graph.getViewport().setMinY(graphYMin);
-        graph.getViewport().setMaxY(graphYMax);
-        graph.getViewport().setScrollable(true);
-
-
-
+        lux = (TextView) findViewById(R.id.lux);
+        maxLux = (TextView) findViewById(R.id.maxLux);
+        minLux = (TextView) findViewById(R.id.minLux);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-
-        Runnable runTimer = new Runnable() {
-            @Override
-            public void run() {
-                graphLastXValue += 1d;
-                graphData.appendData(new DataPoint(graphLastXValue, luxValue), true, graphXMax);
-                graph.addSeries(graphData);
-            }
-        };
-        handler.postDelayed(runTimer, 100);
-
     }
 
     @Override
@@ -82,10 +55,23 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
-            Log.i("Sensor Changed", "onSensor Change :" + event.values[0]);
-            luxValue = event.values[0];
-            tv1.setText(luxValue + " lux");
+            luxValue = (int)event.values[0];
+            lux.setText(String.valueOf(luxValue));
+            listValues.add(luxValue);
+            maxLux.setText(String.valueOf(Collections.max(listValues)));
+            minLux.setText(String.valueOf(Collections.min(listValues)));
+            int avg = average(listValues);
+            Log.i("Avg lux", "" + avg);
         }
+    }
 
+    public static int average(List<Integer> list) {
+        if (list == null || list.isEmpty())
+            return 0;
+        int sum = 0;
+        int n = list.size();
+        for (int i = 0; i < n; i++)
+            sum += list.get(i);
+        return sum / n;
     }
 }
